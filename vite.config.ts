5d060ff1f7b662cd9bc062ee22aec79b05dd8f4b
sdratsab <lold90831@gmail.com>
2026-04-25 12:10:20 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // This is the magic line that makes it work on GitHub Pages
+  // Change the name below to match your repository name exactly
+  base: '/talks-portfolio/', 
 })
